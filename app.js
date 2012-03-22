fc376d872f49d9a8cbc4b3d07a7ed469d3711fe8
Simon McManus <mcmanus.simon@gmail.com>
2012-03-22 18:45:47 +0000

    style updates

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var app = express.createServer();
+var app = express.createServer(express.static(__dirname));
 app.register('.html', require('sizlate'));
 var fs = require('fs');
 
 
 var utf8ify = require('./utf8ify.js');
 
-app.use(express.bodyParser());
 
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.render(__dirname+'/views/index.html', { selectors: {}});
@@ -27,7 +28,7 @@ app.post('/', function(req, res){
 app.get('/utf8ify.js', function(req, res){
 	fs.readFile(__dirname+'/utf8ify.js', 'utf8', function(error, file) {
 		res.send(file);
-	})
+	});
 //    res.download(__dirname+'/utf8ify.js');
 });
 
