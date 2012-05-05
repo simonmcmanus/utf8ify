6ddcbf7f0a917ec437c446688aa3589b3003895f
Simon McManus <mcmanus.simon@gmail.com>
2012-05-05 21:09:06 +0100

    some minor readability tweaks

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 var express = require('express');
-var app = express.createServer(express.static(__dirname));
-app.register('.html', require('sizlate'));  
-var fs = require('fs');
+var app = express.createServer(express['static'](__dirname));
+app.register('.html', require('sizlate'));
 
 var utf8ify = require('./public/utf8ify.js');
 
-app.use(express.bodyParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser()); // required to allow us to read POST data.
+app.use(express['static'](__dirname + '/public')); // set static directory.
 
 
 var render = function(text, res) {
- 	if(text) {
-		var selectors = { 
-			selectors:  {
-			 'textarea': text,
-			 '#result textarea': { value : utf8ify.parse( text , true ) }
-			}
+	var selectors = {};
+	if(text) {
+		selectors = {
+			'textarea': text,
+			'#result textarea': { value : utf8ify.parse( text , true ) }
 		} ;
 	} else {
-		var selectors = { selectors: { '#result': { className: 'hidden' } } };
+		selectors = { '#result': { className: 'hidden' } };
 	}
-	res.render(__dirname+'/views/index.html', selectors);
-}
+	res.render(__dirname+'/views/index.html', { selectors: selectors });
+};
 
 app.get('/', function(req, res){
 	render(req.query['text'], res);
