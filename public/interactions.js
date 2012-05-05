42bc7144edc55a6b1ad59e49e3b847219c623a9f
Simon McManus <mcmanus.simon@gmail.com>
2012-05-05 19:06:32 +0100

    tweaks

--- public/interactions.js
+++ public/interactions.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
 	var $textarea = $('textarea');
-	$textarea.focus();
-$('input[type="submit"]').click(function(e) {
-		e.preventDefault();
-		var text = $textarea.val();
-		history.pushState({}, {}, '/?text=' + text);
-		$textarea.val(utf8ify.parse(text));
-		
-		$('body').append('<div id="notice">Your text has been utf8ified.</div>');
-		$('#notice').css({
-			top: $textarea.offset().top + $textarea.height(),
-			left: $textarea.offset().left,
-			width: $textarea.innerWidth()
-		});
-		$('#notice').fadeIn('fast');
-		setTimeout(function() {
-//			$('#notice').fadeOut('fast');
-		}, 5000);
-		
+//		$textarea.focus();
 
+	$textarea.val($textarea.val().replace('Click the utf8ify button below.', 'Edit this text to see this results below.'));
+	$('.button').remove();
+	var updateTextarea = function(text) {
+		text = text || $textarea.val();
+		$('#result').removeClass('hidden').html('<textarea readonly="readonly">'+utf8ify.parse(text, true)+'</textarea>');
+	};
+	$textarea.keyup(function(){
+		updateTextarea();
+	});
+	$('input[type="submit"]').click(function(e) {
+		e.preventDefault();
+		updateTextarea();
+		return false;
 	});
 });
